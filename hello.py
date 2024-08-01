ddaae2481665cfd8ca988cc6b70d554f66555df7
Neegan000 <131248468+Neegan000@users.noreply.github.com>
2024-08-01 14:09:24 +0545

    Finish project

---
+++ hello.py
@@ -0,0 +1,6 @@
+def hello1():
+  print("hello, World1")
+  
+hello1()
+
+from hello2 import hello2
---
+++ hello2.py
@@ -0,0 +1,6 @@
+def hello2():
+  print("hello, World2")
+
+if __name__ == "__main__":
+  hello2()
+  
